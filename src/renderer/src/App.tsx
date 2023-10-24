e89f5bbf57044901a38097912f73cd2b71d672d4
Tuan Cao <caomanhtuan85@gmail.com>
2023-10-24 08:52:08 +0700

    feat: embed player to show preview of the animation

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,26 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { useState } from 'react'
-
+import { Player } from '@remotion/player'
+import { MyComposition } from '../../remotion/Composition'
 function App(): JSX.Element {
   const [rendered, setRendered] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
   return (
     <div className="container">
+      <Player
+        component={MyComposition}
+        durationInFrames={150}
+        compositionWidth={1920}
+        compositionHeight={1080}
+        fps={30}
+        autoPlay
+        loop
+        controls
+        style={{
+          width: '100%',
+          height: '480px'
+        }}
+      />
       <button
         className="btn"
         style={{ marginTop: '1rem', marginBottom: '1rem', padding: '1rem', fontSize: '1.5rem' }}
