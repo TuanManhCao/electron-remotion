5963b7911b48843c1cb6b538ceeb2891366588df
Tuan Cao <caomanhtuan85@gmail.com>
2023-10-31 17:50:58 +0700

    bundle the composition code into the app itself

--- src/main/bundle-remotion.js
+++ src/main/bundle-remotion.js
@@ -1,14 +1,9 @@
-const { bundle } = require('@remotion/bundler');
-const path = require('path');
-const os = require('os')
-
+const { bundle } = require('@remotion/bundler')
+const path = require('path')
 async function bundle_remotion() {
-  const homeDirectory = os.homedir()
-  const downloadsFolderPath = path.join(homeDirectory, 'Downloads/remotion-bundle/')
   const bundleLocation = await bundle({
     entryPoint: path.resolve('./src/remotion/index.ts'),
-    outDir: downloadsFolderPath,
-    // If you have a Webpack override, make sure to add it here
+    outDir: path.resolve('out/remotion-bundle'),
     webpackOverride: (config) => config
   })
 
--- src/main/renderMedia.ts
+++ src/main/renderMedia.ts
@@ -2,35 +2,26 @@ import { renderMedia, selectComposition } from '@remotion/renderer'
 import { shell } from 'electron'
 import path from 'path'
 import os from 'os'
+import { app } from 'electron'
 // The composition you want to render
 export async function render() {
   const compositionId = 'MyComp'
-  // You only have to create a bundle once, and you may reuse it
-
-  // console.log('🚀 ~ file: renderMedia.ts:14 ~ render ~ bundleLocation:', bundleLocation)
-  // Parametrize the video by passing props to your component.
   const inputProps = {
     foo: 'bar'
   }
-  // Get the composition you want to render. Pass `inputProps` if you
-  // want to customize the duration or other metadata.
   const homeDirectory = os.homedir()
-  const bundled = path.join(homeDirectory, 'Downloads/remotion-bundle/')
+  const bundleDir = path.join(app.getAppPath(), 'out/remotion-bundle')
   const composition = await selectComposition({
-    serveUrl: bundled,
+    serveUrl: bundleDir,
     id: compositionId,
     inputProps
   })
-  console.log('🚀 ~ file: renderMedia.ts:26 ~ render ~ composition:', composition)
-
-  // Render the video. Pass the same `inputProps` again
-  // if your video is parametrized with data.
 
   const downloadsFolderPath = path.join(homeDirectory, 'Downloads/output.mp4')
 
   await renderMedia({
     composition,
-    serveUrl: bundled,
+    serveUrl: bundleDir,
     codec: 'h264',
     outputLocation: downloadsFolderPath,
     inputProps
